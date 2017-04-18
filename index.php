5c582121d5089cae2f36fc3bc36e48177dc9dfb5
Jakub Rychlý <jrychly@jakubrychly.cz>
2017-04-18 21:17:44 +0200

    Kostra webu
    Základní HTML, import externích CSS stylů (Font awesome), grid systém.

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="cs">
+   <head>
+
+      <!-- META -->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="description" content="Jakub Rychlý - vývojář webových aplikací">
+      <meta name="author" content="Jakub Rychlý">
+      <title>Jakub Rychlý</title>
+
+    <!-- Externí CSS -->
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+
+    <!-- Custom CSS -->
+    <link href="css/custom.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="col-4">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+        <div class="col-4">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+        <div class="col-4">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="col-6">
+        Vítám vás na mém webu!
+        </div>
+        <div class="col-6">
+        Web je plně repsonsivní a přízpůsobený na mobilní zařízení.
+        </div>
+    </div>
+
+    <div class="footer">
+    Copyright &copy Jakub Rychlý <?php echo date("Y") ?> | Web hostován u <a href="https://hosting.wedos.com/cs/webhosting.html?ap=136586" target="blank" class="text-upper footer-link">WEDOS</a> | <i class="fa fa-github" aria-hidden="true"></i> <a href="https://github.com/jrychly/jakubrychly.cz" class="footer-link" target="blank">GitHub</a>
+    </div>
+
+    <!-- jQuery Version 3.1.1 -->
+    <script src="js/jquery.js"></script>
+</body>
+
+</html>
