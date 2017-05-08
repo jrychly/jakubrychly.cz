57bfe4dc1336777c8dbd2495a8eae01a5d71e3d3
Jakub Rychlý <jrychly@jakubrychly.cz>
2017-05-08 14:04:40 +0200

    Přidán container s částí "o mně", kde popisuji sebe a to, co dělám. Velké úpravy a přídavky v CSS.

--- css/README.md
+++
@@ -1,2 +0,0 @@
-# Jakub Rychlý - oficiální portfolio
-Kód k mému osobnímu webu, kód není volně šířitelný, ale je dostupný online.
--- index.php
+++ index.php
@@ -5,7 +5,9 @@
       <!-- META -->
       <meta charset="utf-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta content="width=device-width, initial-scale=1" name="viewport">
+      <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+      <link rel="icon" type="image/png" href="img/favicon.png">
       <meta name="description" content="Jakub Rychlý - vývojář webových aplikací">
       <meta name="author" content="Jakub Rychlý">
       <title>Jakub Rychlý</title>
@@ -21,43 +23,111 @@
 <body>
 
     <div class="container">
-        <div class="col-4">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        <div class="col-4 col-center">
+            <div class="panel panel-red">
+                <span class="fa-stack fa-5x">
+                            <i class="fa fa-circle fa-stack-2x text-red"></i>
+                            <i class="fa fa-code fa-stack-1x fa-inverse"></i>
+                        </span>
+                <div class="panel-heading dovednosti"> 
+                    <h4>HTML & CSS</h4>
+                    <p class="dovednost">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    <a href="#fakelink" class="btn btn-red">Red Button</a>
+                </div>
+            </div>
         </div>
-        <div class="col-4">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        <div class="col-4 col-center">
+            <div class="panel panel-blue">
+                <span class="fa-stack fa-5x">
+                            <i class="fa fa-circle fa-stack-2x text-blue"></i>
+                            <i class="fa fa-desktop fa-stack-1x fa-inverse"></i>
+                        </span>
+                <div class="panel-heading dovednosti"> 
+                    <h4>PHP (OOP, MVC a Nette)</h4>
+                    <p class="dovednost">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    <a href="#fakelink" class="btn btn-primary">Primary Button</a>
+                </div>
+            </div>
         </div>
-        <div class="col-4">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        <div class="col-4 col-center">
+            <div class="panel panel-green">
+                <span class="fa-stack fa-5x">
+                            <i class="fa fa-circle fa-stack-2x text-green"></i>
+                            <i class="fa fa-database fa-stack-1x fa-inverse"></i>
+                        </span>
+                <div class="panel-heading dovednosti"> 
+                    <h4>SQL a správa serverů</h4>
+                    <p class="dovednost">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    <a href="#fakelink" class="btn btn-green">Green Button</a>
+                </div>
+            </div>
         </div>
     </div>
 
-    <div class="container">
-        <div class="col-6">
-        Vítám vás na mém webu!
+    <div class="container profile">
+        <div class="col-3 col-center"><img src="https://cdn0.iconfinder.com/data/icons/user-pictures/100/matureman1-512.png" class="img-profile" alt="profile_photo">
+        <h1 class="nadpis-profile">Jakub Rychlý</h1>
+        <a href="#" class="btn btn-info btn-center">Životopis</a>
         </div>
-        <div class="col-6">
-        Web je plně repsonsivní a přízpůsobený na mobilní zařízení.
+        <div class="col-9 text-profile">
+        <h3 class="h2-profile">Kdo jsem</h3>
+        Jsem student a curler z Prahy. Ve volném čase se intenzivně věnuji zímímu olympijskému sportu curling, kromě toho rád programuji v PHP a tvořím v něm webové aplikace. Nadesktopu si hraju s programovacím jazykem Python a nedělá mi problém HTML & CSS, u kterého využívám framework Bootstrap.
+        <h3 class="h2-profile">Co umím</h3>
+        Umím PHP včetně OOP a MVC, HTML & CSS s využitím frameworků Bootstrap a SASS, Python a tvorbu desktopových aplikací v něm, nastavení linuxového serveru a jeho spravování (webový server, TS server, herní server), kromě toho mi nedělá problém střih a editace videa.
+        <h3 class="h2-profile">Co chci umět</h3>
+        Chci umět dokonale vyvíjet software v PHP a ovládat většinu jeho funkcí - ovládnout frameworky jako jsou Nette, Laravel nebo Composer. Dále bych chtěl ovládat plnohodnotně Linuxový server a nemít žádný problém s využíváním Linux na serverech a na desktopech.
+        <h3 class="h2-profile">Co studuji</h3>
+        Studuji Střední průmyslovou školu na Proseku, která se nachází na Praze 9, studuji obor Informační technologie se zaměřením na Linuxového administrátora.
         </div>
     </div>
 
-    <div class="footer">
-    Copyright &copy Jakub Rychlý <?php echo date("Y") ?> | Web hostován u <a href="https://hosting.wedos.com/cs/webhosting.html?ap=136586" target="blank" class="text-upper footer-link">WEDOS</a> | <i class="fa fa-github" aria-hidden="true"></i> <a href="https://github.com/jrychly/jakubrychly.cz" class="footer-link" target="blank">GitHub</a>
+    <div class="container form">
+    <h1 class="h2-form">Kontaktujte mě!</h1>
+        <div class="col-6">
+        <input type="text" placeholder="Jméno a přijmení">
+        </div>
+        <div class="col-6">
+        <input type="text" placeholder="Emailová adresa">
+      </div>
+      <div class="col-12">
+        <textarea placeholder="Vaše zpráva"></textarea>
+      </div>
+    </div>
+    </div>
+
+    <div class="col-12 ender">
+        <div class="col-4 ender-item"><h2>Nejnovější článek na blogu:</h2>
+        <h3><a href="#">Nadpis článku</a></h3><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.</p></div>
+        <div class="col-4 ender-item"><h2>Kde mě najdete:</h2><div class="social-icons">
+                        <a href="https://www.fb.com/kubarychly" target="blank"><span class="fa-stack fa-2x">
+                            <i class="fa fa-circle fa-stack-2x text-facebook"></i>
+                            <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+                        </span></a>
+                        <a href="https://www.twitter.com/@Kubooo85" target="blank"><span class="fa-stack fa-2x">
+                            <i class="fa fa-circle fa-stack-2x text-twitter"></i>
+                            <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+                        </span></a>
+                        <a href="https://www.instagram.com/fastcurling" target="blank"><span class="fa-stack fa-2x">
+                            <i class="fa fa-circle fa-stack-2x text-instagram"></i>
+                            <i class="fa fa-instagram fa-stack-1x fa-inverse"></i>
+                        </span></a>
+                        <a href="https://www.linkedin.com/in/jakub-rychl%C3%BD-92b401134/" target="blank"><span class="fa-stack fa-2x">
+                            <i class="fa fa-circle fa-stack-2x text-linkedin"></i>
+                            <i class="fa fa-linkedin fa-stack-1x fa-inverse"></i>
+                        </span></a>
+                        <a href="http://github.com/jrychly" target="blank"><span class="fa-stack fa-2x">
+                            <i class="fa fa-circle fa-stack-2x text-github"></i>
+                            <i class="fa fa-github fa-stack-1x fa-inverse"></i>
+                        </span></a>
+                        </div></div>
+        <div class="col-4 ender-item"><h2>Moje projekty:</h2>
+        <a href="http://murkasoft.cz" target="blank"><img src="img/ms_logo.png" alt="murkasoft_logo"></a>
+        </div>
+    </div>
+
+    <div class="col-12 footer">
+    Copyright &copy; Jakub Rychlý 2014 - <?php echo date("Y") ?> | Web hostován u <a href="https://hosting.wedos.com/cs/webhosting.html?ap=136586" target="blank" class="text-upper footer-link">WEDOS</a> | <i class="fa fa-github" aria-hidden="true"></i> <a href="https://github.com/jrychly/jakubrychly.cz" class="footer-link" target="blank">GitHub</a>
     </div>
 
     <!-- jQuery Version 3.1.1 -->
